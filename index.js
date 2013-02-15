23c2546a6c5400b79684c6babe6e2e6c5e023c80
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-15 15:58:20 -0600

    optimized classification routine

--- index.js
+++ index.js
@@ -6,23 +6,35 @@ module.exports = function(grid, faceNormals, x) {
   if(!result) {
     return Number.NaN;
   }
-  var d = numeric.sub(x, result.points[0]);
-  var l = numeric.norm2(d);
-  if(l < EPSILON) {
+  if(result.distance < EPSILON) {
     return 0.0;
   }
-  numeric.muleq(d, 1.0 / Math.sqrt(l));
-  var cells = result.cells;
   var best_n = 0.0;
+  var polarity = false;
+  var cells = result.cells;
+  var points = result.points;
   for(var i=0; i<cells.length; ++i) {
-    var n = faceNormals[cells[i]];
-    var w = numeric.dot(n, d);
-    if(Math.abs(w) > Math.abs(best_n)) {
-      best_n = w;
+    var l = 0.0;
+    var n = 0.0;
+    var p = points[i];
+    var fn = faceNormals[cells[i]];
+    for(var j=0; j<x.length; ++j) {
+      var d = p[j] - x[j];
+      l += d * d;
+      n += fn[j] * d;
+    }
+    n /= Math.sqrt(l);
+    if(n < 0) {
+      n = -n;
+      if(n > best_n) {
+        best_n = n;
+        polarity = true;
+      }
+    } else if(n > best_n) {
+      best_n = n;
+      polarity = false;
     }
   }
-  if(best_n < 0) {
-    return -result.distance;
-  }
-  return result.distance;
+  var r = Math.sqrt(result.distance);
+  return polarity ? r : -r;
 }
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var mesh = require("conway-hart")("C");
-var grid = require("spatial-grid")(mesh, 0.5);
-var faceNormals = require("normals").faceNormals(mesh.positions, mesh.faces);
+var grid = require("spatial-grid")(mesh.cells, mesh.positions, 0.5);
+var faceNormals = require("normals").faceNormals(mesh.cells, mesh.positions);
 var signedDistance = require("../index.js");
 
 for(var x=0.0; x<1.0; x+=0.1) {
